d5bae1738ad634a5c9d117910252b7e342857153
GigAByte <alijuinorabbasi@gmail.com>
2023-10-17 13:06:47 +0100

    adding

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
-from flask import flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 with open('post.json') as file:
   blog_posts = json.load(file)
 
-app = flask(__name__)
+# def main():
+#    print(blog_posts)
+
+def update_blog_posts(author, title, content):
+   id:int = blog_posts[-1]['id'] + 1
+   post = {
+      'id' : id,
+      'author' : author,
+      'title' : title,
+      'content' : content 
+   }
+   blog_posts.append(post)
+   
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     # add code here to fetch the job posts from a file
     return render_template('index.html', posts=blog_posts)
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+      author = request.form.get('author')
+      title = request.form.get('title')
+      content = request.form.get('content')
+      update_blog_posts(author, title, content)
+      return redirect(url_for('index'))
+    return render_template('add.html')
+
+
 if __name__ == '__main__':
   app.run(debug=True)
+  # main()
